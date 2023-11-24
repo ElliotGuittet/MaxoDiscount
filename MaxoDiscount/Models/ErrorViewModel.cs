cfa99eeffdb1056e5444fbb3eb94d7a788168411
ElliotGuittet <73930698+ElliotGuittet@users.noreply.github.com>
2023-11-24 12:17:14 +0100

    Départ

---
+++ MaxoDiscount/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MaxoDiscount.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
